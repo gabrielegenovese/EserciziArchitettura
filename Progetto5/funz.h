b5b8449302c680e1a43a40422008e058e06e7624
gabrigeno <gabrigeno@gmail.com>
2020-12-03 18:22:10 +0100

    final push

---
+++ Progetto5/funz.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct variabile{
+    char *nome;
+    int val;
+    struct variabile *next;
+} *VAR;
+
+char *nome_file(char str[]);
+char *getCommand(char str[128]);
+void write_bit(FILE * fhack, int write[16]);
+int *num_to_bit(int n);
+void error(char *c, int riga);
+int isStringEqual(char * p1, char * p2);
+int assegnamento(int *write, char c, char *op);
+void exec_command(char *command, int riga, int write[16], FILE * fhack, VAR var);
+VAR newVariable(VAR head, char * name, int val);
+VAR getLabel(VAR variables, FILE * fasm);
+VAR getVar(VAR variables, FILE * fasm);
